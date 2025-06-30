f13a52bf70186ceb38e4e4ec0e2bd33a471da06c
wasss4bi <wasss4bi@yandex.ru>
2025-06-30 11:17:12 +0600

    fix

--- src/views/role/_form.php
+++ src/views/role/_form.php
@@ -19,29 +19,30 @@ $checkedArr = $model->id == 1 ? ['checked' => true] : [];
             <button type="button" class="btn btn-sm btn-outline-primary select-all">Выбрать все</button>
             <button type="button" class="btn btn-sm btn-outline-danger deselect-all">Убрать все</button>
         </div>
-        <div class="row">
-            <?php
-            foreach ($model->allPermissions as $controller => $actions): ?>
-                <div class="col-md-6">
-                    <div class="card mb-4">
-                        <div class="card-header bg-light">
-                            <strong><?= end(explode("\\", $controller)) ?></strong>
-                            <div class="d-flex justify-content-end">
-                                <button type="button" class="btn btn-sm btn-outline-primary select-all-on-controller">Выбрать все</button>
-                                <button type="button" class="btn btn-sm btn-outline-danger deselect-all-on-controller">Убрать все</button>
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            <?php
-                            foreach ($actions as $id => $action):
-                                echo $form->field($model, "checkbox_permissions[$id]")->checkbox(array_merge(['label' => $action, 'class' => 'permission-checkbox', 'disabled' => $model->id === 1], $checkedArr));
-                            endforeach; ?>
+    <?php endif; ?>
+
+    <div class="row">
+        <?php
+        foreach ($model->allPermissions as $controller => $actions): ?>
+            <div class="col-md-6">
+                <div class="card mb-4">
+                    <div class="card-header bg-light">
+                        <strong><?= end(explode("\\", $controller)) ?></strong>
+                        <div class="d-flex justify-content-end">
+                            <button type="button" class="btn btn-sm btn-outline-primary select-all-on-controller">Выбрать все</button>
+                            <button type="button" class="btn btn-sm btn-outline-danger deselect-all-on-controller">Убрать все</button>
                         </div>
                     </div>
+                    <div class="card-body">
+                        <?php
+                        foreach ($actions as $id => $action):
+                            echo $form->field($model, "checkbox_permissions[$id]")->checkbox(array_merge(['label' => $action, 'class' => 'permission-checkbox', 'disabled' => $model->id === 1], $checkedArr));
+                        endforeach; ?>
+                    </div>
                 </div>
-            <?php endforeach; ?>
-        </div>
-    <?php endif; ?>
+            </div>
+        <?php endforeach; ?>
+    </div>
     <div class="form-group">
         <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
         <?= Html::a('Вернуться к списку', ['index'], ['class' => 'btn btn-default']) ?>
